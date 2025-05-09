57fc0121daabeb7c126eeebdd9e8c925ffbc1b7c
zarah <devvick230385@gmail.com>
2025-05-09 21:15:53 +0100

    fix: change `verified` status to true after successful verification

--- src/api/v1/services/auth/AuthService.js
+++ src/api/v1/services/auth/AuthService.js
@@ -181,9 +181,8 @@ exports.verify = async (id) => {
       }
 
       await account.deployAccount(account_payload);
-
-      await user.update({ verified: true });
     }
+    await user.update({ verified: true });
     const accessToken = await GenerateToken(user.id);
     await Token.deleteVerificationTokens(user.id);
     return CheckDBResponse.successResponse({
