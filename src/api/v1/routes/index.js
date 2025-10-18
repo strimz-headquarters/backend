fcea353b67ebe4ec3a1fbd1b30ac2e837fa2ca6c
zarah <devvick230385@gmail.com>
2025-10-18 14:48:50 +0100

    chore: connect all routes to main

--- src/api/v1/routes/index.js
+++ src/api/v1/routes/index.js
@@ -5,12 +5,20 @@ const AuthRoute = require("./auth/AuthRoute");
 const UserRoute = require("./user/UserRoute");
 const UtilitiesRoute = require("./utilities/UtilitiesRoute");
 const PayrollRoute = require("./payroll/PayrollRoute");
+const MerchantRoute = require("./gateway/MerchantRoute");
+const PaymentRoute = require("./gateway/PaymentRoute");
+const TransactionRoute = require("./gateway/TransactionRoute");
+const WebhookRoute = require("./gateway/WebhookRoute");
 // const PlanRoute = require("./plan/PlanRoute");
 
 router.use("/auth", AuthRoute);
 router.use("/users", UserRoute);
 router.use("/utilities", UtilitiesRoute);
 router.use("/payroll", PayrollRoute);
+router.use("/merchant", MerchantRoute);
+router.use("/payment", PaymentRoute);
+router.use("/transaction", TransactionRoute);
+router.use("/webhook", WebhookRoute);
 // router.use("/plan", PlanRoute);
 
 module.exports = router;
